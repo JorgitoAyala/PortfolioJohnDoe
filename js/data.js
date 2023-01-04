57217b6b5b88325d20ced178ea99b612c9af039d
Jorge Ayala <74685456+JorgitoAyala@users.noreply.github.com>
2023-01-04 17:10:03 -0500

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,46 @@
+//DATA
+const arrMainInfo = [
+  {
+    date: "2005 - 2006",
+    name: "Graphic",
+    img: "https://images.pexels.com/photos/322338/pexels-photo-322338.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    company: "Amazon",
+  },
+  {
+    date: "2010 - 2015",
+    name: "Website",
+    img: "https://images.pexels.com/photos/1034646/pexels-photo-1034646.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    company: "Google",
+  },
+  {
+    date: "2017 - 2018",
+    name: "UI / UX",
+    img: "https://images.pexels.com/photos/389819/pexels-photo-389819.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    company: "Facebook",
+  },
+  {
+    date: "2019 - 2020",
+    name: "Product",
+    img: "https://images.pexels.com/photos/326516/pexels-photo-326516.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    company: "Twitter",
+  },
+  {
+    date: "2020 - 2022",
+    name: "App",
+    img: "https://images.pexels.com/photos/326513/pexels-photo-326513.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    company: "Microsoft",
+  },
+  {
+    date: "2022 - Present",
+    name: "Concept",
+    img: "https://images.pexels.com/photos/2857477/pexels-photo-2857477.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    company: "Tesla",
+  },
+];
+
+const arrJobs = [
+  "https://images.pexels.com/photos/927451/pexels-photo-927451.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  "https://images.pexels.com/photos/925786/pexels-photo-925786.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  "https://images.pexels.com/photos/859264/pexels-photo-859264.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  /* "https://images.pexels.com/photos/926390/pexels-photo-926390.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",*/
+];
